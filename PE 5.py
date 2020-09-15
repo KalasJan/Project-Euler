1dfefa14ee92703469bbba1f0ad42eed4f1ffcd2
KalasJan <70596881+KalasJan@users.noreply.github.com>
2020-09-15 19:48:02 +0200

    Add files via upload

---
+++ PE 5.py
@@ -0,0 +1,10 @@
+# Project Euler 5
+
+# What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+# primes - 2, 3, 5, 7, 11, 13, 17, 19
+# other: 4 = 2*2, 6 = 2*3, 8 = 2*2*2, 10 = 5*2, 12 = 3*2*2
+# 14 = 7*2, 15 = 3*5, 16 = 2*2*2*2, 18 = 3*3*2, 20 = 5*2*2
+
+print (2*3*5*7*11*13*17*19*2*2*2*3)
+
