858e4818ca759617d01c58d0d1767cb929a288e7
KalasJan <70596881+KalasJan@users.noreply.github.com>
2020-09-16 12:59:18 +0200

    Update PE 4.py

--- PE 4.py
+++ PE 4.py
@@ -7,4 +7,4 @@ for x in range (900, 1000): # we want to have biggest nubmer - so we use interva
         n = x*y
         if str(n) == str(n)[::-1]: # definition of palindromes
             print (n)
-            # we have 12 solution, we choose the biggist of them
+            # we have 12 solutions, we choose the biggest of them
